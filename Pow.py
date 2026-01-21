0073a8a40f85d8182fe928f07dd8cc5a31b72e1c
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-01-21 02:27:53 -0700

    Create Pow.py
    Signed-off-by: Richard Kindler <223166946+RickCreator87@users.noreply.github.com>

---
+++ Pow.py
@@ -0,0 +1,6 @@
+import type { PowDocument } from '@gitdigital/proof-of-contribution-protocol-core';
+// or from './src/schema/types';
+
+const powDoc: PowDocument = {
+  // ... strongly-typed PoW structure here
+};
